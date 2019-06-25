6da1367bae09f9b6e3e8a6e304b2cc512bcdf463
shivaconceptsolution <50239824+shivaconceptsolution@users.noreply.github.com>
2019-06-25 18:39:15 +0530

    Add files via upload

---
+++ helloword/src/main/java/com/scs/entryleval/Hello.java
@@ -0,0 +1,26 @@
+package com.scs.entryleval;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+ 
+public class Hello {
+ 
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+ 
+		// loading the definitions from the given XML file
+		ApplicationContext context = new ClassPathXmlApplicationContext(
+				"applicationContext.xml");
+ 
+		HelloWorldService service = (HelloWorldService) context
+				.getBean("helloWorldService");
+		String message = service.sayHello();
+		System.out.println(message);
+ 
+		//set a new name
+		service.setName("Spring");
+		message = service.sayHello();
+		System.out.println(message);
+	}
+}
---
+++ helloword/src/main/java/com/scs/entryleval/HelloWorldService.java
@@ -0,0 +1,17 @@
+package com.scs.entryleval;
+
+import org.springframework.stereotype.Service;
+
+@Service("helloWorldService")
+public class HelloWorldService {
+ 
+	private String name;
+ 
+	public void setName(String name) {
+		this.name = name;
+	}
+ 
+	public String sayHello() {
+		return "Hello! " + name;
+	}
+}
